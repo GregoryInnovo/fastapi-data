dd64a08f2c5a9c0fc4b834e2916a1f80feb6ffd8
Ubuntu <ubuntu@ip-172-31-35-165.us-west-2.compute.internal>
2025-07-31 23:33:50 +0000

    transaction update

--- app/routers/transaction_router.py
+++ app/routers/transaction_router.py
@@ -179,7 +179,13 @@ def create_transaction(transaction: TransactionCreate, db: Session = Depends(get
                 noches=info.noches,
                 incluye=info.incluye,
                 no_incluye=info.no_incluye,
-                transaction_id=new_transaction.id
+                transaction_id=new_transaction.id,
+                alimentacion = travel_info.alimentacion,
+                acomodacion =  travel_info.acomodacion,
+                direccion_hotel = travel_info.direccion_hotel,
+                pais_destino = travel_info.pais_destino,
+                ciudad_destino = travel_info.ciudad_destino
+
             )
             db.add(travel_info)
     # if transaction.evidence and transaction.evidence.evidence_file[:5] != "https":
