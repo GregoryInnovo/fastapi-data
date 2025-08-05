88c2f71272dbb0c8d515b1a1cc3808ff2aff87f1
Ubuntu <ubuntu@ip-172-31-35-165.us-west-2.compute.internal>
2025-08-05 04:31:44 +0000

    evidence

--- app/routers/transaction_router.py
+++ app/routers/transaction_router.py
@@ -261,7 +261,6 @@ def create_transaction(transaction: TransactionCreate, db: Session = Depends(get
             evidencias = Evidence(
                 evidence_file=evidence.evidence_file,
                 amount=evidence.amount,
-                upload_date=evidence.upload_date,
                 status=evidence.status,
                 transaction_id=new_transaction.id
             )
